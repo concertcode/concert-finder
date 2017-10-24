65769a4e4468c18655a6cbc17282aff48ac07570
Peter Peck <karatekid4christ@gmail.com>
2017-10-24 14:58:30 -0500

    Setting up Ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,11 @@ $('.datepicker').pickadate({
 	closeOnSelect: false // Close upon selecting a date,
 });
 
+
  $(document).ready(function() {
 
  	//Getting the values from the user
- 	$("#submit").on("click", function(){
-
- 		event.preventDefault();
+ 	function eventful() {
 
  		//Value from user for artist
  		var artistname = $("#artist-name").val().trim();
@@ -28,10 +27,29 @@ $('.datepicker').pickadate({
  		var zipcode = parseInt($("#zip-code").val())
 		
 		//Value from user for distance radius
-		var distanceradius = $("#distance-form").val(); 		
-		
+		var distanceradius = parseInt($("#distance-form").val()); 		
 
- 	});
+ 	 
+	// var url = "http://api.eventful.com/json/performers/search?";
+	// var apikey = "app_key=dXWwC4cHg4gX4NfZ&";
+	// var search = "keywords=" + artistname;
+	// var position = "&where=32.746682,-117.162741"
+	// // var distance = "&within=" + distanceradius;
+ // 	var queryURL = url + apikey + search + position + queryURL;
+ 		var queryURL = 'https://api.eventful.com/json/performers/search?app_key=dXWwC4cHg4gX4NfZ&keywords=korn&where=32.746682,-117.162741&within=100'
 
+ 		
+      //The AJAX function for pulling data from the API
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response){
+      	console.log(response)
+      });
 
- 	});
+	};
+
+	//When the submit button is clicked, it calls the eventful function
+    $(document).on("click", "#submit", eventful);
+
+ });
