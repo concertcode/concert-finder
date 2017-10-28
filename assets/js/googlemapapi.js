e5fa4297698bbb643981afaa81c023161565853d
Peter Peck <karatekid4christ@gmail.com>
2017-10-28 18:23:28 -0500

    Organzing google map javascript

--- assets/js/googlemapapi.js
+++ assets/js/googlemapapi.js
@@ -51,59 +51,76 @@
 
 
 
+$( document ).ready(function() {
 
-      var map, infoWindow;
-      function initMap() {
+    var map;
+    var infoWindow = new google.maps.InfoWindow;
+
+    function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 37.0902, lng: -95.7129},
+        center: {lat: 37.0902, lng: -95.7129},
+        zoom: 3
+    });
+    
 
-          zoom: 3
-        });
-        infoWindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+    };
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            };
+    var marker = new google.maps.Marker({
+    position: pos,
+    map: map,
+    title: 'Your Location'
+    });
 
-            var marker = new google.maps.Marker({
-              position: pos,
-              map: map,
-              title: 'Your Location'
-             });
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
 
-            var infowindow = new google.maps.InfoWindow();
-            var infowindowContent = document.getElementById('infowindow-content');
-            infowindow.setContent(infowindowContent);
+    map.setZoom(10);
+    map.setCenter(marker.getPosition());
+    infowindow.open(map, marker);
+    infowindowContent.children['place-name'].textContent = "Your Location";
+    infowindow.open(map, marker);
 
-                map.setZoom(10);
-                map.setCenter(marker.getPosition());
-                infowindow.open(map, marker);
-                infowindowContent.children['place-name'].textContent = "Your Location";
-                infowindow.open(map, marker);
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(marker);
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent(marker);
-            
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+    map.setCenter(pos);
+    }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+    });
+    } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+    }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+    }
+
+    $(document).on("click", ".map-button", function() {
+        // Get latitude value
+        var longCoord = $(this).attr("latitude")
+        // Get longitude value
+        var latCoord = $(this).attr("longitude")
+
+        var concertCoord = {
+            lat: latCoord,
+            lng: longCoord,
         }
-      }
+    })
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
 
-      
+
+
+
+});
