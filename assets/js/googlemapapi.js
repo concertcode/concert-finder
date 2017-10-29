90a673571b134b2d8770effd1d4202cbe585dba4
Peter Peck <karatekid4christ@gmail.com>
2017-10-29 12:55:25 -0500

    Trying to go to dest coords for map

--- assets/js/googlemapapi.js
+++ assets/js/googlemapapi.js
@@ -1,127 +1,141 @@
-      var map, infoWindow;
-     function initMap() {
+var map, infoWindow;
 
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var directionsService = new google.maps.DirectionsService;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: {lat: 37.77, lng: -122.447}
-        });
+function initMap(latitudeconcert, longconcert) {
 
-        infoWindow = new google.maps.InfoWindow;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
 
-        // Try HTML5 geolocation.
-        /*if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-              zoom: 15
-            };
+    var directionsService = new google.maps.DirectionsService;
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            // map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }*/
+    var map = new google.maps.Map(document.getElementById('map'), {
 
-        directionsDisplay.setMap(map);
+        zoom: 14,
 
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-        document.getElementById('mode').addEventListener('change', function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        });
-      }
+        center: {lat: 37.77, lng: -122.447}
 
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    });
 
-        var latitudeconcert = $(this).attr("latitude");
-        console.log(latitudeconcert)
+    infoWindow = new google.maps.InfoWindow;
 
-        var longconcert = $(this).attr("longitude");
-        console.log(longconcert)
+// Try HTML5 geolocation.
+/*if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function(position) {
+var pos = {
+lat: position.coords.latitude,
+lng: position.coords.longitude,
+zoom: 15
+};
 
-        // var pos = {
-        //     lat: latitudeconcert,
-        //     lng: longconcert,
-        // }   
+infoWindow.setPosition(pos);
+infoWindow.setContent('Location found.');
+infoWindow.open(map);
+// map.setCenter(pos);
+}, function() {
+handleLocationError(true, infoWindow, map.getCenter());
+});
+} else {
+// Browser doesn't support Geolocation
+handleLocationError(false, infoWindow, map.getCenter());
+}*/
 
-        // var myJSON = JSON.stringify(pos)
+    directionsDisplay.setMap(map);
 
-        var selectedMode = document.getElementById('mode').value;
-        directionsService.route({
-          origin: {lat: 41.9211, lng: -87.6340},  // Haight.
-          destination: {lat: latitudeconcert, lng: longconcert},  // Ocean Beach.
-          // Note that Javascript allows us to access the constant
-          // using square brackets and a string value as its
-          // "property."
-          travelMode: google.maps.TravelMode[selectedMode]
-        }, function(response, status) {
-          if (status == 'OK') {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
+    calculateAndDisplayRoute(directionsService, directionsDisplay, latitudeconcert, longconcert);
 
+// document.getElementById('mode').addEventListener('change', function() {
 
-      // function initMap() {
-      //   map = new google.maps.Map(document.getElementById('map'), {
-      //     center: {lat: 37.0902, lng: -95.7129},
+// });
+}
 
-      //     zoom: 15
-      //   });
-      //   infoWindow = new google.maps.InfoWindow;
+function calculateAndDisplayRoute(directionsService, directionsDisplay,latitudeconcert, longconcert) {
 
-      //   // Try HTML5 geolocation.
-      //   if (navigator.geolocation) {
-      //     navigator.geolocation.getCurrentPosition(function(position) {
-      //       var pos = {
-      //         lat: position.coords.latitude,
-      //         lng: position.coords.longitude,
-      //         zoom: 15
-      //       };
+    console.log(typeof latitudeconcert);
 
-      //       infoWindow.setPosition(pos);
-      //       infoWindow.setContent('Location found.');
-      //       infoWindow.open(map);
-      //       map.setCenter(pos);
-      //     }, function() {
-      //       handleLocationError(true, infoWindow, map.getCenter());
-      //     });
-      //   } else {
-      //     // Browser doesn't support Geolocation
-      //     handleLocationError(false, infoWindow, map.getCenter());
-      //   }
-      // }
+    console.log(typeof longconcert);
 
-      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      //   infoWindow.setPosition(pos);
-      //   infoWindow.setContent(browserHasGeolocation ?
-      //                         'Error: The Geolocation service failed.' :
-      //                         'Error: Your browser doesn\'t support geolocation.');
-      //   infoWindow.open(map);
-      // }
-      
+    var selectedMode = document.getElementById('mode').value;
+
+    directionsService.route({
+
+    origin: {lat: 41.9994210, lng: -87.6658960},  // Haight.
+
+    destination: {lat: latitudeconcert, lng: longconcert},  // Ocean Beach.
+
+    // Note that Javascript allows us to access the constant
+    // using square brackets and a string value as its
+    // "property."
+
+    travelMode: google.maps.TravelMode[selectedMode]
+
+    }, function(response, status) {
+
+    if (status == 'OK') {
+
+        directionsDisplay.setDirections(response);
+
+    } else {
+
+        window.alert('Directions request failed due to ' + status);
+
+    }
+
+    });
+}
 
 
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 37.0902, lng: -95.7129},
+
+//     zoom: 15
+//   });
+//   infoWindow = new google.maps.InfoWindow;
+
+//   // Try HTML5 geolocation.
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude,
+//         zoom: 15
+//       };
+
+//       infoWindow.setPosition(pos);
+//       infoWindow.setContent('Location found.');
+//       infoWindow.open(map);
+//       map.setCenter(pos);
+//     }, function() {
+//       handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+// }
+
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//                         'Error: The Geolocation service failed.' :
+//                         'Error: Your browser doesn\'t support geolocation.');
+//   infoWindow.open(map);
+// }
 
 
-      $(document).on("click", ".map-button", function(){
 
-        $("#floating-panel").show();
 
-        initMap();
 
-      })
+$(document).on("click", ".map-button", function(){
 
-      
+    var latitudeconcert = $(this).attr("latitude");
+
+    var longconcert = $(this).attr("longitude");
+
+    $("#floating-panel").show();
+
+    initMap(latitudeconcert, longconcert);
+
+})
+
+
 
 
