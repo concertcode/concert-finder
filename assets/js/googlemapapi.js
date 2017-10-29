75d2b91a24d582b65661de4379b453b0848fa5f8
Peter Peck <karatekid4christ@gmail.com>
2017-10-29 12:13:21 -0500

    Adding gelocation function

--- assets/js/googlemapapi.js
+++ assets/js/googlemapapi.js
@@ -1,4 +1,5 @@
-    function initMap() {
+      var map, infoWindow;
+     function initMap() {
 
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var directionsService = new google.maps.DirectionsService;
@@ -6,6 +7,31 @@
           zoom: 14,
           center: {lat: 37.77, lng: -122.447}
         });
+
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+              zoom: 15
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            // map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        infoWindow = new google.maps.InfoWindow;
         directionsDisplay.setMap(map);
 
         calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -31,7 +57,49 @@
           }
         });
       }
+
+
+      // function initMap() {
+      //   map = new google.maps.Map(document.getElementById('map'), {
+      //     center: {lat: 37.0902, lng: -95.7129},
+
+      //     zoom: 15
+      //   });
+      //   infoWindow = new google.maps.InfoWindow;
+
+      //   // Try HTML5 geolocation.
+      //   if (navigator.geolocation) {
+      //     navigator.geolocation.getCurrentPosition(function(position) {
+      //       var pos = {
+      //         lat: position.coords.latitude,
+      //         lng: position.coords.longitude,
+      //         zoom: 15
+      //       };
+
+      //       infoWindow.setPosition(pos);
+      //       infoWindow.setContent('Location found.');
+      //       infoWindow.open(map);
+      //       map.setCenter(pos);
+      //     }, function() {
+      //       handleLocationError(true, infoWindow, map.getCenter());
+      //     });
+      //   } else {
+      //     // Browser doesn't support Geolocation
+      //     handleLocationError(false, infoWindow, map.getCenter());
+      //   }
+      // }
+
+      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      //   infoWindow.setPosition(pos);
+      //   infoWindow.setContent(browserHasGeolocation ?
+      //                         'Error: The Geolocation service failed.' :
+      //                         'Error: Your browser doesn\'t support geolocation.');
+      //   infoWindow.open(map);
+      // }
       
+
+
+
 
       $(document).on("click", ".map-button", function(){
 
@@ -41,7 +109,7 @@
 
         $("#floating-panel").show();
 
-        initMap(latitudeconcert, longconcert);
+        initMap();
 
       })
 
