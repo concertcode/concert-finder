27657d264999908dfd6885e073c967a15a5ad875
Peter Peck <karatekid4christ@gmail.com>
2017-10-28 23:06:54 -0500

    Updating googlemapapi.js file

--- assets/js/googlemapapi.js
+++ assets/js/googlemapapi.js
@@ -51,6 +51,11 @@
 
 
     function initMap() {
+
+        var latitudeconcert = $(this).attr("latitude");
+
+        var longconcert = $(this).attr("longitude");
+         
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var directionsService = new google.maps.DirectionsService;
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -68,8 +73,8 @@
       function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         var selectedMode = document.getElementById('mode').value;
         directionsService.route({
-          origin: {lat: 37.77, lng: -122.447},  // Haight.
-          destination: {lat: 37.768, lng: -122.511},  // Ocean Beach.
+          origin: {lat: , lng: },  // Haight.
+          destination: {lat: latitudeconcert, lng: longconcert},  // Ocean Beach.
           // Note that Javascript allows us to access the constant
           // using square brackets and a string value as its
           // "property."
@@ -85,7 +90,7 @@
       
 
       $(document).on("click", ".map-button", function(){
-        $("floating-panel").show();
+        $("#floating-panel").show();
         initMap();
       })
 
