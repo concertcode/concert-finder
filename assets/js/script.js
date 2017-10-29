e4f2a7c5999d8631cd5376a09afed2471dd776f6
Victor <deep-research>
2017-10-29 00:12:55 -0500

    Added semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,38 +16,38 @@ $(document).ready(function() {
 	// Submission code block
  	$("#submit").on("click", function(){
  		// The table is made visable and cleared
- 		$("#concert-table").show()
- 		$("#concert-results").html("")
+ 		$("#concert-table").show();
+ 		$("#concert-results").html("");
 
  		// Form values retrieved
- 		try {var artistName = $("#artist-name").val().trim();} catch(err) {}
-	    try {var zipCode = $("#zip-code").val().trim();} catch(err) {}
-	    try {var distanceRadius = $("#distance-form").val().trim();} catch(err) {}
+ 		try {var artistName = $("#artist-name").val().trim();} catch(err) {};
+	    try {var zipCode = $("#zip-code").val().trim();} catch(err) {};
+	    try {var distanceRadius = $("#distance-form").val().trim();} catch(err) {};
 
 	    // last.fm url queries
-	    var lastFmUrl = "https://ws.audioscrobbler.com/2.0/?"
-	    var lastFmApiKey = "&api_key=afc4afb74959db18d42a677803c3ac59"
-	    var lastFmJson = "&format=json"
-	    var similarBandsLimit = "&limit=" + "50"
- 		var searchArtistQuery = lastFmUrl + "method=artist.getinfo&artist=" + artistName + lastFmApiKey + lastFmJson
- 		var similarArtistQuery = lastFmUrl + "method=artist.getsimilar&artist=" + artistName + lastFmApiKey + similarBandsLimit + lastFmJson
+	    var lastFmUrl = "https://ws.audioscrobbler.com/2.0/?";
+	    var lastFmApiKey = "&api_key=afc4afb74959db18d42a677803c3ac59";
+	    var lastFmJson = "&format=json";
+	    var similarBandsLimit = "&limit=" + "50";
+ 		var searchArtistQuery = lastFmUrl + "method=artist.getinfo&artist=" + artistName + lastFmApiKey + lastFmJson;
+ 		var similarArtistQuery = lastFmUrl + "method=artist.getsimilar&artist=" + artistName + lastFmApiKey + similarBandsLimit + lastFmJson;
 
  		// Get name and image of main band from last.fm
 	    $.ajax({
 	        url: searchArtistQuery,
 	        method: 'GET'
 	    }).done(function(response) {
-	    	var bandName = response.artist.name
+	    	var bandName = response.artist.name;
 		    $("#display-band").html(bandName);
 
-		    var bandImage = response.artist.image[5]['#text']
+		    var bandImage = response.artist.image[5]['#text'];
 		    $(".card-image").html("<img src=" + bandImage + "/img>");
 	    });
 
 	    // Form appearance reset after submission
-	    $("label").attr("class", "white-text")
-	    $("input:text").val("")
-	    $(".select-dropdown").val("Distance")
+	    $("label").attr("class", "white-text");
+	    $("input:text").val("");
+	    $(".select-dropdown").val("Distance");
 
 	    // Eventful API function
 	    var eventfulApi = function(band) { 
@@ -70,40 +70,47 @@ $(document).ready(function() {
 		    		// Loop through each event if there are any
 			    	for (var i=0; i < response.events.event.length; i++) {
 			    		// Get event data
+
+			    		// Event Title
 			    		try {
 			    			var title = response.events.event[i].title;
 			    		} catch(err) {
 			    			var title = "title missing";
 			    		}
 
+			    		// City Name
 			    		try {
 			    			var city = response.events.event[i].city_name;
 			    		} catch(err) {
 			    			var city = "city missing";
 			    		}
 
+			    		// Venue Name
 			    		try {
 			    			var venue = response.events.event[i].venue_name;
 			    		} catch(err) {
 			    			var venue = "venue missing";
 			    		}
 
+			    		// Event Date
 			    		try {
 			    			var date = response.events.event[i].start_time.substring(0, 10);
 			    		} catch(err) {
-			    			var date = "date missing"
+			    			var date = "date missing";
 			    		}
 
+			    		// Event Latitude
 			    		try {
-			    			var latitude = response.events.event[i].latitude
+			    			var latitude = response.events.event[i].latitude;
 			    		} catch(err) {
-			    			var latitude = "latitude missing"
+			    			var latitude = "latitude missing";
 			    		}
 
+			    		// Event Longitude
 			    		try {
-			    			var longitude = response.events.event[i].longitude
+			    			var longitude = response.events.event[i].longitude;
 			    		} catch(err) {
-			    			var longitude = "longitude missing"
+			    			var longitude = "longitude missing";
 			    		}
 
 			    		// Display event data in table
@@ -116,10 +123,10 @@ $(document).ready(function() {
 													 "<td>" + date + "</td>" +
 													 "<td><input class='map-button' type='button' name='map-button' value='Map' " +
 													 "latitude='" + latitude + "' longitude='" + longitude + "'></td>" +
-													 "</tr>")
+													 "</tr>");
 			    		};
 			    		// Update results counter
-			    		$("#result-counter").html("Results: " + rowCount)
+			    		$("#result-counter").html("Results: " + rowCount);
 			    	};
 			    } catch(err) {
 			    }
@@ -127,10 +134,10 @@ $(document).ready(function() {
 			    // Tell the user if there are no events
 			    var rowCount = $('table tr').length;
 				if (rowCount === 1) {
-					$("#result-counter").html("")
-					$("#concert-results").html("<td id='empty-table'>No results!</td>")
+					$("#result-counter").html("");
+					$("#concert-results").html("<td id='empty-table'>No results! Try again.</td>")
 				} else if (rowCount > 1) {
-					$("#empty-table").remove()
+					$("#empty-table").remove();
 				}
 			}); // Eventful ajax
 		} // Eventful function
@@ -141,7 +148,7 @@ $(document).ready(function() {
 	        method: 'GET'
 	    }).done(function(response) {
 	    	// Start an array with the main band
-	    	var similarBandsList = [artistName]
+	    	var similarBandsList = [artistName];
 
 	    	// Add similar bands to the array
 	    	for (var i=1; i < response.similarartists.artist.length; i++) {
@@ -155,4 +162,3 @@ $(document).ready(function() {
 	    });
  	}); // Click submit button
 });
-
